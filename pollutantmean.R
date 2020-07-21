9c44c374f9091641178cd36904eb546494d34860
Timi Okah <debetimi@gmail.com>
2020-07-21 13:37:14 -0700

    wtfR

--- pollutantmean.R
+++ pollutantmean.R
@@ -1,13 +1,9 @@
 pollutantmean <- function(directory, pollutant, id=1:332) {
     filenames = dir(directory, full.names = T)
     output = read.csv(filenames[id[1]])
-    output <- output[complete.cases(output), ]
-    print(dim(output))
     for(file in filenames[id[-1]]) {
-        print(file)
         df <- read.csv(file, )
-        output <- rbind(output, df[complete.cases(df), ])
+        output <- rbind(output, df)
     }
-    print(dim(output))
-    mean(output[[pollutant]])
+    mean(output[complete.cases(output[pollutant]),][[pollutant]])
 }
