dfb1186abd436fcae3cd54427aee1d1adcce2b53
Timi Okah <debetimi@gmail.com>
2020-07-21 14:56:41 -0700

    yeet

--- complete.R
+++ complete.R
@@ -1,11 +1,6 @@
 complete <- function(directory, id=1:332) {
-    answer <- data.frame(id=id, nobs=rep(0, length(id)))
-    for (n in 1:length(id)) {
-        filename <- paste(sprintf("%03d", id[n]), ".csv", sep="")
-        fp <- paste(directory, filename, sep="/")
-        df <- read.csv(fp)
-        df2 <- df[complete.cases(df), ]
-        answer[["nobs"]][n] = dim(df2)[1]
-    }
-    answer
+    filenames = dir(directory, full.names = T)
+    reducer <- function(x,y){append(x, dim(y[complete.cases(y),])[1])}
+    nobs <- Reduce(reducer,lapply(filenames[id], read.csv),init=c());
+    data.frame(id=id, nobs=nobs)
 }
